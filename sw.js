e6134b357db724aa2545cffea3b5e79778e7f4d9
Luisfch1 <ingenierocuellar1@gmail.com>
2026-01-02 12:20:45 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // sw.js
-const CACHE_VERSION = "logi2-v0.7.2s2c";
+const CACHE_VERSION = "logi2-v0.7.2s2d";
 const CACHE = `logi2-cache-${CACHE_VERSION}`;
 
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./manifest.webmanifest?v=0.7.2s2c",
-  "./favicon.png?v=0.7.2s2c",
-  "./apple-touch-icon.png?v=0.7.2s2c",
+  "./manifest.webmanifest?v=0.7.2s2d",
+  "./favicon.png?v=0.7.2s2d",
+  "./apple-touch-icon.png?v=0.7.2s2d",
   "./Logi2_Plantilla_Items.xlsx",
 
   "./favicon.png",
@@ -75,7 +75,7 @@ self.addEventListener("fetch", (event) => {
 
     try {
       const fresh = await fetch(req);
-      if (fresh && fresh.ok) await cache.put(req, fresh.clone());
+      if (fresh && (fresh.ok || fresh.type === "opaque")) await cache.put(req, fresh.clone());
       return fresh;
     } catch {
       return Response.error();
