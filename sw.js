5b2aeae5d6e87b1f514323873daa2d995afcbafc
Luisfch1 <ingenierocuellar1@gmail.com>
2026-01-03 20:51:07 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // sw.js
-const CACHE_VERSION = "logi2-v0.7.2s2e";
+const CACHE_VERSION = "logi2-v0.7.2s2f";
 const CACHE = `logi2-cache-${CACHE_VERSION}`;
 
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./manifest.webmanifest?v=0.7.2s2e",
-  "./favicon.png?v=0.7.2s2e",
-  "./apple-touch-icon.png?v=0.7.2s2e",
+  "./manifest.webmanifest?v=0.7.2s2f",
+  "./favicon.png?v=0.7.2s2f",
+  "./apple-touch-icon.png?v=0.7.2s2f",
   "./Logi2_Plantilla_Items.xlsx",
 
   "./favicon.png",
