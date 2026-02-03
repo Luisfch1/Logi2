23d501490a1324d0858d16078eb5f7235af25488
Luisfch1 <ingenierocuellar1@gmail.com>
2026-02-03 16:20:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // Incrementa la versión en cada deploy para invalidar caché
-const CACHE_VERSION = "logi2-v0.8.7.24";
+const CACHE_VERSION = "logi2-v0.8.7.25";
 const CACHE = `logi2-cache-${CACHE_VERSION}`;
 
 const ASSETS = [
